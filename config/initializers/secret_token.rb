fe2dbb1172d10675dd0bf35cefa3090068042b39
pranavbhalla89 <Apple@Apples-MacBook-Pro.local>
2014-01-03 20:41:56 -0500

    ror first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RORFirstApp::Application.config.secret_key_base = '433444b13767fcc5b8dfe3911ea178da8a52d4facc826b90971a5ae57fee85adfcc40e8e65c714bd7756f1658b3cb2fe542c0dd8587b64f9890b8d6a08e47e73'
